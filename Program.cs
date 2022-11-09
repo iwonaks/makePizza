293c37d5032aa598a6831b5b8e41f6539b05fa0a
iwonaks <iwonaks@gmail.com>
2022-11-09 14:55:36 +0100

    correction1

--- Program.cs
+++ Program.cs
@@ -1,32 +1,33 @@
 ﻿using System.Threading.Tasks.Dataflow;
 
-var stack = new Stack<double>();
+var weightComponent = new Stack<double>();
 
-var stackstring = new Stack<string>();
+var component = new Stack<string>();
 
 
 Console.WriteLine("Podstawowy przepis na 1 dużą pizzę zawiera 500g mąki (ok 2 pełne szklanki mąki)");
 Console.WriteLine("");
 
 do
-{   stack.Push(15);
-    stack.Push(5);
-    stack.Push(13);
-    stack.Push(10);
-    stack.Push(350);
-    stack.Push(500);
+{
+    weightComponent.Push(15);
+    weightComponent.Push(5);
+    weightComponent.Push(13);
+    weightComponent.Push(10);
+    weightComponent.Push(350);
+    weightComponent.Push(500);
 
-    stackstring.Push("Oliwa: ");
-    stackstring.Push("cukier: ");
-    stackstring.Push("sól: ");
-    stackstring.Push("świeże drożdże piekarskie: ");
-    stackstring.Push("woda: ");
-    stackstring.Push("mąka typu 00: ");
+    component.Push("Oliwa: ");
+    component.Push("cukier: ");
+    component.Push("sól: ");
+    component.Push("świeże drożdże piekarskie: ");
+    component.Push("woda: ");
+    component.Push("mąka typu 00: ");
 
     Console.WriteLine("---------------------------------------------");
     Console.WriteLine("Podaj ile gram mąki posiadasz. Program poda Ci ile potrzebujesz pozostałych składników. /n Wagę podaj w gramach, równą lub więszą od 125g (ok pół szklanki)");
    
-    string input = Console.ReadLine();
+    string? input = Console.ReadLine();
 
     if (input != null)
     {   
@@ -38,10 +39,10 @@ do
                 double convert = many / 500;
                 Console.WriteLine("Potrzebujesz:");
 
-                while (stack.Count > 0 && stackstring.Count > 0)
+                while (weightComponent.Count > 0 && component.Count > 0)
                 {
-                    double item = stack.Pop() * convert;
-                    string itemstring = stackstring.Pop();
+                    double item = weightComponent.Pop() * convert;
+                    string itemstring = component.Pop();
 
                     Console.WriteLine($"{itemstring} {item:N1} g");
                 }
@@ -49,23 +50,23 @@ do
             else
             {
                 Console.WriteLine("{0} jest wartością poniżej 125 g, nikt nie poje sobie taką pizzą", many);
-                stack.Clear();
-                stackstring.Clear();
+                weightComponent.Clear();
+                component.Clear();
             }
         }
 
         catch (System.OverflowException)
         {
             Console.WriteLine("Bez żartów, program ma sens dla wartości od 125g mąki");
-            stack.Clear();
-            stackstring.Clear();
+            weightComponent.Clear();
+            component.Clear();
         }
 
         catch (System.FormatException)
         {
             Console.WriteLine("Wpisałeś niepoprawną wratość");
-            stack.Clear();
-            stackstring.Clear();
+            weightComponent.Clear();
+            component.Clear();
         }
     }
 }
